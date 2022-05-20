fd7f93b2423381d9d8d22fe1ee84dee26c2d7d37
조경진 <kmc901206@gmail.com>
2022-05-20 16:25:26 +0900

    - Bottom Nav / ViewPager2   1. xml에 배치 id를 부여
      2. res > menu > bottom Nav에 연결
    
      3. ViewPager2 생성 / Bottom Nav 표시
    
      4. ViewPager2 / Bottom Nav 연동

--- app/src/main/java/com/neppplus/a20220520_todolist_recyclerview/MainActivity.kt
+++ app/src/main/java/com/neppplus/a20220520_todolist_recyclerview/MainActivity.kt
@@ -2,6 +2,7 @@ package com.neppplus.a20220520_todolist_recyclerview
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.viewpager2.widget.ViewPager2
 import com.neppplus.a20220520_todolist_recyclerview.adapters.MainViewPagerAdapter
 import kotlinx.android.synthetic.main.activity_main.*
 
@@ -23,5 +24,38 @@ class MainActivity : AppCompatActivity() {
     private fun setValues() {
         mPagerAdapter = MainViewPagerAdapter(this)
         mainViewPage.adapter = mPagerAdapter
+
+//        바텀 네이게이션 클릭 이벤트 처리
+        bottomNav.setOnItemSelectedListener {
+            when (it.itemId) {
+                R.id.home -> mainViewPage.currentItem = 0
+                R.id.paper -> mainViewPage.currentItem = 1
+                R.id.profile -> mainViewPage.currentItem = 2
+            }
+            return@setOnItemSelectedListener true
+        }
+
+//        뷰 페이저 연결
+        mainViewPage.registerOnPageChangeCallback(object : ViewPager2.OnPageChangeCallback(){
+            override fun onPageSelected(position: Int) {
+                super.onPageSelected(position)
+                bottomNav.menu.getItem(position).isChecked = true
+            }
+        })
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
